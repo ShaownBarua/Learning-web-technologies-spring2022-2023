8405c600668a012889c87a5f828610950bdeee45
ShaownBarua <87904273+ShaownBarua@users.noreply.github.com>
2023-02-28 02:00:14 +0600

    Add files via upload

---
+++ LAB-2-PHP/area.php
@@ -0,0 +1,16 @@
+<html>
+
+    <?php
+    $length=5;
+    $width=6;
+    $area=$length*$width;
+	
+    echo "Area of rectangle is =".$area."<br>";
+
+    $perimeter=2*($length+$width);
+    echo "perimeter of rectangle is =".$perimeter;
+    ?>
+	
+	
+	
+</html>
